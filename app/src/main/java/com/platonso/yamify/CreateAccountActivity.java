6235e4442821faa7c87cbf1ed3c34a2323cda701
platonso <plat100916@gmail.com>
2024-05-07 22:09:36 +0300

    added auth

---
+++ app/src/main/java/com/platonso/yamify/CreateAccountActivity.java
@@ -0,0 +1,119 @@
+package com.platonso.yamify;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_account);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.create), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        createAccountBtn.setOnClickListener(v-> createAccount());
+        loginBtnTextView.setOnClickListener(v-> finish());  //возврат
+    }
+
+    void createAccount() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email, password, confirmPassword);
+        if (!isValidated) {
+            return;
+        }
+
+        createAccountFirebase(email,password);
+    }
+
+
+    void createAccountFirebase(String email, String password){
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changeInProgress(false);
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateAccountActivity.this,
+                                    "Successfully create account", Toast.LENGTH_SHORT).show();
+
+                        }else {
+                            Toast.makeText(CreateAccountActivity.this,task.getException()
+                                    .getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    boolean validateData(String email, String password, String confirmPassword){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password is too short");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            passwordEditText.setError("Password not matched");
+            return false;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/platonso/yamify/MainActivity.kt
+++ app/src/main/java/com/platonso/yamify/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
